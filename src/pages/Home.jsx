b0490805c1277377b203b2532b0653a7c26229a0
yeison ospino <jeison1500@hotmail.com>
2023-06-20 23:10:33 -0500

    responsive.3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
     <main>
 
     
-      <section className="flex flex-col items-center justify-center h-screen">
+      <section className="flex flex-col items-center justify-center h-screen bg-black">
         <div className="p-2">
           <img src="/images/logo.png" alt="" />
         </div>
@@ -37,15 +37,15 @@ const Home = () => {
         <h3 className="text-3xl text-red-500 mt-20 font-bold  ">
           !Hello trainer¡
         </h3>
-        <p className="text-black text-2xl font-bold mt-4">
+        <p className="text-white text-2xl font-bold mt-4">
           For start, give your name
         </p>
         
 
         <section className="flex flex-col items-center justify-center mt-8 ">
         <form onSubmit={handleSubmit}>          
-          <input required id="nameTrainer"  className="bg-slate-50 border-b-4 py-4 text-center text-md" type="text" placeholder="Name" />
-          <button className="bg-red-600 text-white py-4 mt-4 px-4" >
+          <input required id="nameTrainer"  className="bg-slate-50  py-4  text-md" type="text" placeholder="Name" />
+          <button className="bg-red-600 text-white py-4 mt-4 px-4 text-md " >
             Start
           </button>
         </form>
